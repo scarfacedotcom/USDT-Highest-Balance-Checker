2a690fbdcb5294b1c154520f8e285154990c4740
scarfacedotcom <peteradaaku@gmail.com>
2023-02-26 13:13:30 +0100

    few changes

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,9 @@
 import fetch from "node-fetch";
 
 async function main() {
-  const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+  const usdt_contract_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
   const holders = 1;
-  const url = `https://api.ethplorer.io/getTopTokenHolders/${usdt}?apiKey=freekey&limit=${holders}`;
+  const url = `https://api.ethplorer.io/getTopTokenHolders/${usdt_contract_address}?apiKey=freekey&limit=${holders}`;
   const response = await fetch(url);
   const data = await response.json();
   if (data && data.holders && data.holders.length > 0) {
